4b4f42a17d11340936c690b1a51f370b708dad79
SHIAK AFRID <afridayan01@gmail.com>
2025-04-11 09:28:23 +0000

    Add initial components for Home, Login, and Sign Up pages; update package dependencies

---
+++ src/features/home/HomeMain.tsx
@@ -0,0 +1,11 @@
+function HomeMain(){
+    return (
+        <div className="home-main">
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the main content area of the home page.</p>
+        </div>
+    );
+}
+
+
+export default HomeMain
