7aa8c866b1f21558a55b772f17ac5fc5f8b72a6f
SHIAK AFRID <afridayan01@gmail.com>
2025-04-11 11:14:22 +0000

    Added home page and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Protected from "./apputils/Protected";
+import UnProtected from "./apputils/UnProtected";
+import HomeMain from "./features/home/HomeMain";
 
 
 function App() {
-
+  const queryClient = new QueryClient();
   return (
-    <>
-     <div className='text-red-400 '>Hello wolrd</div>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<UnProtected />}>
+            <Route path="/" index element={<HomeMain />} />
+          </Route>
+
+          <Route element={<Protected />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/apputils/Protected.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+function Protected() {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+}
+
+export default Protected;
