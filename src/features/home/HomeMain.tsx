998033297d9c31e22235460d0085376bda4aa127
SHIAK AFRID <afridayan01@gmail.com>
2025-04-14 01:55:34 +0530

    Update HomeMain.tsx

--- src/features/home/HomeMain.tsx
+++ src/features/home/HomeMain.tsx
@@ -238,10 +238,10 @@ function HomeMain() {
    <div className="flex flex-col gap-12 p-6 md:p-12 max-w-7xl mx-auto">
       {/* Banner 1 - Trusted Globally */}
       <div>
-        <div className="flex flex-col md:flex-row items-center bg-[#0b0d39] text-white rounded-xl overflow-hidden">
+        <div className="flex flex-col md:flex-row items-center bg-[#060314] text-white rounded-xl overflow-hidden">
           <div className="md:w-1/2 p-8">
-            <p className="text-3xl mb-2">Grow your business</p>
-            <h2 className="text-4xl md:text-7xl font-semibold mb-4 leading-snug">
+            <p className="text-xl mb-2 text-black/60">Grow your business</p>
+            <h2 className="lg:text-4xl text-3xl font-semibold mb-4 leading-snug">
               Trusted globally by over 1 million businesses, small to large
             </h2>
             <button className="bg-orange-500 hover:bg-orange-600 text-white font-medium px-6 py-3 rounded mt-4">
@@ -272,21 +272,21 @@ function HomeMain() {
           <img
             src="/home/small.jpg"
             alt="Freelancer woman"
-            className="w-full h-full object-fill "
+            className="w-full h-full object-cover "
           />
         </div>
         <div className="md:w-1/2 bg-blue-600 text-white p-8 flex flex-col">
           <div className=" p-4">
             <div className="mb-7">
-              <p className="text-lg mb-2">For talent</p>
-              <h2 className="text-6xl font-semibold mb-4">Find great work</h2>
-              <p className="mb-6 text-white/90 text-2xl">
+              <p className="text-lg mb-2 text-black/60">For talent</p>
+              <h2 className="text-4xl font-semibold mb-4">Find great work</h2>
+              <p className="mb-6 text-white/90 text-lg">
                 Meet clients you’re excited to work with and take your career or
                 business to new heights.
               </p>
             </div>
             <hr className="border-white/30 mb-6" />
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-white/90 text-2xl">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-white/90 text-lg">
               <div>
                 <p>
                   Find opportunities for every stage of your freelance career
@@ -314,9 +314,9 @@ function HomeMain() {
         />
         <div className="absolute inset-0 bg-black/50 flex flex-col justify-center px-6 sm:px-10 md:px-16 py-10 text-white">
           <div className="max-w-2xl">
-            <p className="text-4xl sm:text-6xl mb-2">For clients</p>
+            <p className="text-lg mb-2 text-black/60">For clients</p>
             <div className="mt-12 sm:mt-24">
-              <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold leading-tight mb-4">
+              <h2 className="text-3xl font-semibold leading-tight mb-4">
                 Find talent your way
               </h2>
               <p className="text-lg sm:text-xl md:text-2xl mb-6 sm:mb-8">
@@ -326,8 +326,8 @@ function HomeMain() {
             </div>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full p-4 sm:p-6 md:p-10 mt-auto">
-            <div className="bg-green-600 hover:bg-green-700 p-4 md:p-6 rounded-lg transition text-white flex flex-col justify-between h-full">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full p-4 sm:p-6 md:p-10 ">
+            <div className="bg-[#208507] p-4 md:p-6 rounded-lg transition text-white flex flex-col justify-between h-full">
               <div>
                 <h3 className="text-2xl sm:text-3xl md:text-4xl font-semibold mb-2">
                   Post a job and hire a pro
@@ -337,7 +337,7 @@ function HomeMain() {
                 Talent Marketplace<span>→</span>
               </p>
             </div>
-            <div className="bg-green-600 hover:bg-green-700 p-4 md:p-6 rounded-lg transition text-white flex flex-col justify-between h-full">
+            <div className="bg-[#208507] p-4 md:p-6 rounded-lg transition text-white flex flex-col justify-between h-full">
               <div>
                 <h3 className="text-2xl sm:text-3xl md:text-4xl font-semibold mb-2">
                   Browse and buy projects
@@ -347,7 +347,7 @@ function HomeMain() {
                 Project Catalog <span>→</span>
               </p>
             </div>
-            <div className="bg-green-600 hover:bg-green-700 p-4 md:p-6 rounded-lg transition text-white flex flex-col justify-between h-full">
+            <div className="bg-[#208507] p-4 md:p-6 rounded-lg transition text-white flex flex-col justify-between h-full">
               <div>
                 <h3 className="text-2xl sm:text-3xl md:text-4xl font-semibold mb-2">
                   Get advice from an industry expert
