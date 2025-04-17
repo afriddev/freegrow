b63d3a3be777ab48e62e2c5157e4fac1ca21061a
revuzT <revuz.cnc@gmail.com>
2025-04-17 06:32:45 +0530

    Scroll
    Added Scroll Provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,25 @@ import UnProtected from "./apputils/UnProtected";
 import HomeMain from "./features/home/HomeMain";
 import SignUp from "./features/signup/SignUp";
 import Login from "./features/login/LoginMain";
+import LenisProvider from "./lib/LenisProvider";
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<UnProtected />}>
-            <Route path="/" index element={<HomeMain />} />
-            <Route path="/signup" index element={<SignUp />} />
-            <Route path="/login" index element={<Login />} />
-          </Route>
+      <LenisProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<UnProtected />}>
+              <Route path="/" index element={<HomeMain />} />
+              <Route path="/signup" index element={<SignUp />} />
+              <Route path="/login" index element={<Login />} />
+            </Route>
 
-          <Route element={<Protected />}></Route>
-        </Routes>
-      </BrowserRouter>
+            <Route element={<Protected />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </LenisProvider>
     </QueryClientProvider>
   );
 }
