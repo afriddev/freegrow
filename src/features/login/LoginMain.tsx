5210e42500deaf8aef4d754ab170dc86ef29da78
revuzT <revuz.cnc@gmail.com>
2025-04-17 06:23:48 +0530

    Login
    Added Image and Icons in Login

--- src/features/login/LoginMain.tsx
+++ src/features/login/LoginMain.tsx
@@ -1,11 +1,12 @@
 import LoginSocials from "./LoginSocials";
 import DividerWithText from "@/apputils/DividerWithText";
 import LoginForm from "./LoginForm";
+import LoginIcons from "./LoginIcons";
 
 function Login() {
   return (
     <section className="h-screen flex p-1 md:p-2 bg-gray-200 ">
-      <div className="border-2 w-full md:w-[57%] pl-[1%] md:pl-[2%] flex items-center flex-col justify-between rounded-lg bg-white ">
+      <div className="border-2 w-full md:w-[50%] pl-[1%] md:pl-[2%] flex items-center flex-col justify-between rounded-lg bg-white ">
         <div className="font-medium w-full flex justify-start mt-[4%] gap-x-2 items-center">
           <img src="public/favicon.png" width="18" />
           FreeGrow
@@ -19,11 +20,24 @@ function Login() {
           <DividerWithText title={"OR"} />
           <LoginForm />
         </div>
-        <div className="w-full pb-[4%] flex justify-start text-xs xl:text-sm">
+        <div className="w-full pb-[4%] flex flex-col justify-start font-medium text-xs xl:text-sm">
+          <div className="md:hidden flex mb-2">
+            <LoginIcons white={false} />
+          </div>
           &copy;2025 FreeGrow. All rights reserved.
         </div>
       </div>
-      <div className="border-2 md:flex hidden"></div>
+      <div
+        className="border-2 md:flex hidden w-full md:w-[50%] rounded-lg bg-cover bg-center"
+        style={{
+          backgroundImage:
+            "url('https://images.pexels.com/photos/14374368/pexels-photo-14374368.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')",
+        }}
+      >
+        <div className="w-full flex items-end justify-end mr-5 mb-4">
+          <LoginIcons white={true} />
+        </div>
+      </div>
     </section>
   );
 }
